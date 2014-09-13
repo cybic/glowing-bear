b1ca4b661d1eaa626245778ea364b5f9f602107b
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2014-09-13 19:59:32 +0100

    Implement buffer history for alt+a and alt+<, alt+>, and alt+/

--- js/inputbar.js
+++ js/inputbar.js
@@ -143,6 +143,7 @@ weechat.directive('inputBar', function() {
 
                 // Support different browser quirks
                 var code = $event.keyCode ? $event.keyCode : $event.charCode;
+                var keydown = $event.type === "keydown";
 
                 // any other key than Tab resets nick completion iteration
                 var tmpIterCandidate = $scope.iterCandidate;
@@ -190,7 +191,7 @@ weechat.directive('inputBar', function() {
                 // Alt+A -> switch to buffer with activity
                 if ($event.altKey && (code === 97 || code === 65)) {
                     $event.preventDefault();
-                    $rootScope.switchToActivityBuffer();
+                    models.switchToActivityBuffer();
                     return true;
                 }
 
@@ -202,14 +203,23 @@ weechat.directive('inputBar', function() {
                     return true;
                 }
 
-                // Alt+< -> switch to previous buffer
+                // Alt+< -> go back in history
                 if ($event.altKey && (code === 60 || code === 226)) {
-                    var previousBuffer = models.getPreviousBuffer();
-                    if (previousBuffer) {
-                        models.setActiveBuffer(previousBuffer.id);
-                        $event.preventDefault();
-                        return true;
-                    }
+                    $event.preventDefault();
+                    return models.switchToPrevNextBuffer(false);
+                }
+
+                // Alt+> -> go forward in history
+                // TODO alternative codes for cross-browser stuff
+                if ($event.altKey && code === 62) {
+                    $event.preventDefault();
+                    return models.switchToPrevNextBuffer(true);
+                }
+
+                // Alt+/ -> switch to previous buffer
+                if ($event.altKey && code === 47) {
+                    $event.preventDefault();
+                    return models.switchToLastBuffer();
                 }
 
                 // Double-tap Escape -> disconnect
@@ -244,7 +254,7 @@ weechat.directive('inputBar', function() {
                 }
 
                 // Arrow up -> go up in history
-                if ($event.type === "keydown" && code === 38) {
+                if (keydown && code === 38) {
                     $scope.command = models.getActiveBuffer().getHistoryUp($scope.command);
                     // Set cursor to last position. Need 0ms timeout because browser sets cursor
                     // position to the beginning after this key handler returns.
@@ -257,7 +267,7 @@ weechat.directive('inputBar', function() {
                 }
 
                 // Arrow down -> go down in history
-                if ($event.type === "keydown" && code === 40) {
+                if (keydown && code === 40) {
                     $scope.command = models.getActiveBuffer().getHistoryDown($scope.command);
                     // We don't need to set the cursor to the rightmost position here, the browser does that for us
                     return true;
